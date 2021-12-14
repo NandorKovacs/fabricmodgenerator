129428063b7aef6d123bfa7f19c645dc46e10361
Roaringmind22 <nandor1kovacs@gmail.com>
2021-12-14 20:01:57 +0100

    fixed mixins json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## [1.0.0]
 
 - Initial release
+
+## [1.0.1]
+
+- fixed modid.mixins.json accidentally beeing named modid.mod.json
--- src/extension.ts
+++ src/extension.ts
@@ -58,7 +58,7 @@ function chooseFolder(folderName: string) {
   });
 }
 
-function createAndClone(pathStr: string, folderName: string) {  
+function createAndClone(pathStr: string, folderName: string) {
   fs.mkdir(pathStr, { recursive: true }).then((pPathAsOr) => {
     if (pPathAsOr !== undefined) {
       let clone = require("git-clone/promise");
@@ -129,7 +129,7 @@ function initClass(pathStr: string, mainPackageStr: string, modidStr: string, fo
 
 function modifySrc(pathStr: string, mainPackageStr: string, modidStr: string, initClassStr: string) {
   let javaPath = path.join(pathStr, "src", "main", "java");
-  
+
   fs.readFile(
     path.join(javaPath, packageToPath("net.fabricmc.example"), "ExampleMod.java"),
     { encoding: "ascii" }).then(
@@ -246,7 +246,7 @@ function configureMixinJson(pathStr: string, mainPackageStr: string, modidStr: s
 
       fs.rm(path.join(resourcePath,"modid.mixins.json")).then(
         () => {
-          fs.writeFile(path.join(resourcePath, modidStr + ".mod.json"), strJson, {encoding: "ascii"}).then(
+          fs.writeFile(path.join(resourcePath, modidStr + ".mixins.json"), strJson, {encoding: "ascii"}).then(
             () => {
               return;
             }, () => {
